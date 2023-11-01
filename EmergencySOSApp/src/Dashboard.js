6f2867df409bcf3ae47a24c474c8b365caae3d4a
nageshchari <nageshchari75@gmail.com>
2023-11-02 01:04:18 +0530

    DashBoard

--- EmergencySOSApp/src/Dashboard.js
+++ EmergencySOSApp/src/Dashboard.js
@@ -3,12 +3,25 @@ import * as React from 'react';
 import { View, Text, } from 'react-native';
 import { red, white } from './Constants';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Image} from 'react-native';
 
 
 function Dashboard() {
   return (
     <View style={{ flex: 1, alignItems: 'center' }}>
-      <Text style={{ color: 'red', fontSize: 64,  marginTop: 20 }}>Welcome</Text>
+      <Text style={{ color: 'red', fontSize: 64,  marginTop: 30, marginBottom: 30, fontWeight: 'bold' }}>Welcome</Text>
+      <Image
+        style={{height:150, width:150, alignSelf:'center', marginTop: 50}}
+        source={require('./assets/Contacts.png')}
+      />
+      <Image
+        style={{height:150, width:150, alignSelf:'center', marginTop: 50}}
+        source={require('./assets/Flash.png')}
+      />
+      <Image
+        style={{height:150, width:150, alignSelf:'center', marginTop: 50}}
+        source={require('./assets/Instructions.png')}
+      />
     </View>
   );
 }
